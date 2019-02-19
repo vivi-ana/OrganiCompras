32e77e8f1f4787bda50fe41802587a4d2f804384
Viviana <acostaviviana.a@gmail.com>
2019-02-19 01:19:17 -0300

    Movimiento del icono terminado

--- app/src/main/java/acostapeter/com/organicompras/SplashScreen.java
+++ app/src/main/java/acostapeter/com/organicompras/SplashScreen.java
@@ -4,21 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 public class SplashScreen extends Activity { //extiende de una actividad para que puedo ser un splash screen
-ImageView icono;
-Animation animacionIconoIzquierda, animacionIconoDerecha;
-AnimationSet desplazamiento = new AnimationSet(false);//usar dos animaciones
+ImageView icono, nombreapp;
+TranslateAnimation animacionDerecha, animacionIzquierda, animacionNombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         icono = findViewById(R.id.iconImage);
+        nombreapp = findViewById(R.id.iconoNombre);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -27,29 +25,31 @@ AnimationSet desplazamiento = new AnimationSet(false);//usar dos animaciones
                 finish();
             }
         }, 3000);
-        icono.setVisibility(View.VISIBLE);
-        animacionIconoDerecha = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.animacion_icono_derecha);
-        desplazamiento.addAnimation(animacionIconoDerecha);
-        icono.startAnimation(desplazamiento);
-
-        animacionIconoDerecha.setAnimationListener(new Animation.AnimationListener() {
+        animacionNombre = new TranslateAnimation(1200,0,0,0);
+        animacionNombre.setDuration(1500);
+        nombreapp.setAnimation(animacionNombre);
+        animacionDerecha = new TranslateAnimation(1200,-1000,0,0);
+        animacionDerecha.setDuration(1200);
+        icono.setRotationY(180f); //efecto espejo en el icono
+        animacionDerecha.setFillEnabled(true);
+        animacionDerecha.setFillAfter(true);
+        icono.startAnimation(animacionDerecha);
+        animacionDerecha.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-
             }
 
             @Override
-            public void onAnimationEnd(Animation arg0) {
-
-                animacionIconoIzquierda = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.animacion_icono_izquierda);
-                desplazamiento.addAnimation(animacionIconoIzquierda);
-                icono.startAnimation(desplazamiento);
-
+            public void onAnimationEnd(Animation animation) {
+                animacionIzquierda = new TranslateAnimation(-1000,0,0,0);
+                animacionIzquierda.setDuration(1500);
+                icono.setRotationY(360f);
+                animacionIzquierda.setFillEnabled(true);
+                animacionIzquierda.setFillAfter(true);
+                icono.startAnimation(animacionIzquierda);
             }
-
             @Override
             public void onAnimationRepeat(Animation animation) {
-
             }
         });
     }
