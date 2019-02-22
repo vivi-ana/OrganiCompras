2868ed4218f5ee8a82b9e73928f0e9b37be4b552
Viviana <acostaviviana.a@gmail.com>
2019-02-22 00:00:14 -0300

    Update import

--- app/src/main/java/acostapeter/com/organicompras/MainActivity.java
+++ app/src/main/java/acostapeter/com/organicompras/MainActivity.java
@@ -1,12 +1,10 @@
 package acostapeter.com.organicompras;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.CardView;
