8afb3a584a08f817410ebb20c24e90b9636c164f
Viviana <acostaviviana.a@gmail.com>
2019-02-23 14:11:38 -0300

    Pager implementado pero no terminado

--- app/src/main/java/acostapeter/com/organicompras/Tabs.java
+++ app/src/main/java/acostapeter/com/organicompras/Tabs.java
@@ -1,7 +1,17 @@
 package acostapeter.com.organicompras;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tabs extends AppCompatActivity {
 
@@ -9,5 +19,71 @@ public class Tabs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Compras"));
+        tabLayout.addTab(tabLayout.newTab().setText("Despensa"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+                return false;
+            }
+        };
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void mensajeVacio() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(Tabs.this);
+        alertBuilder.setTitle(R.string.salirVacio);
+        alertBuilder.setCancelable(false);
+        alertBuilder.setMessage(R.string.msjSalirVacio);
+        alertBuilder.setPositiveButton(R.string.si, new DialogInterface.OnClickListener() {
+            @Override
+            //Si acepta el mensaje
+            public void onClick(DialogInterface dialog, int which) {
+                //Si se queda a agregar productos. se cierra el mensaje.
+            }
+        });
+        alertBuilder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Si cancela el mensaje tiene que borrar y salir.
+              // borrar();
+                finish();
+            }
+        });
+        Dialog dialog = alertBuilder.create();
+        dialog.show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
