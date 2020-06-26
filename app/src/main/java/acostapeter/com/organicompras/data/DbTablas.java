a85b79f30274ef90d6263c61b67c4f2b29ff5c9f
Viviana <acostaviviana.a@gmail.com>
2020-06-25 22:25:00 -0300

    fix compras

--- app/src/main/java/acostapeter/com/organicompras/data/DbTablas.java
+++ app/src/main/java/acostapeter/com/organicompras/data/DbTablas.java
@@ -105,16 +105,16 @@ class DbTablas {
             + TablaProdXSuper.CAMPO_PRECIO + " real not null ) ; ";
 
     final String EXISTENTES_CREATE = "create table " + TablaExistentes.TABLA_EXISTENTES + " ( "
-            + TablaExistentes.CAMPO_ID_EXISTENTE + " text primary key not null, "
+            + TablaExistentes.CAMPO_ID_EXISTENTE + " text not null, "
             + TablaExistentes.CAMPO_FK_ID_PROD + " integer not null ) ; ";
 
     final String DESPENSANOPROD_CREATE = "create table " + TablaProductosCasa.TABLA_PROD_CASA + " ( "
-            + TablaProductosCasa.CAMPO_ID_CASA + " text primary key not null, "
+            + TablaProductosCasa.CAMPO_ID_CASA + " text not null, "
             + TablaProductosCasa.CAMPO_FK_ID_PROD + " integer not null , "
             + TablaProductosCasa.CAMPO_GUARDAR + " text not null ) ; ";
 
     final String SUPERNOPROD_CREATE = "create table " + TablaProductosSuper.TABLA_PROD_NO_EN_COMP + " ( "
-            + TablaProductosSuper.CAMPO_ID_PROD_SUPER + " text primary key not null, "
+            + TablaProductosSuper.CAMPO_ID_PROD_SUPER + " text not null, "
             + TablaProductosSuper.CAMPO_FK_ID_SUPER + " integer not null , "
             + TablaProductosSuper.CAMPO_FK_ID_PROD + " integer not null , "
             + TablaProductosSuper.CAMPO_PRECIO + " real not null ) ; ";
