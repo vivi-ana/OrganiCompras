306323bfd5dd7f7da30370709dfc4a3f0c0941f5
Viviana <acostaviviana.a@gmail.com>
2020-08-26 21:08:44 -0300

    Fix no cargar el ultimo

--- app/src/main/java/acostapeter/com/organicompras/Compras.java
+++ app/src/main/java/acostapeter/com/organicompras/Compras.java
@@ -197,6 +197,7 @@ private DbCRUD admin;
     }
     String total_productos_comprados(){
         String cantidad_productos;
+        cant_total_productos = 0;
         cantidad_productos = admin.cantidad_productos_comprados(id);
         if(cantidad_productos!=null)
         cant_total_productos = Integer.parseInt(cantidad_productos);
